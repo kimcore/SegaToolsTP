76256ef68d3240f2ec3df24447ee90b6f8fe9d53
nzgamer41 <pslover9@gmail.com>
2020-03-10 22:00:40 +1300

    i hate appveyor

--- idz.ps1
+++ idz.ps1
@@ -1,7 +1,4 @@
-﻿  Get-Content .\idzhook\resource.rc | ForEach-Object { $_ -replace "1.0.0.0", $env:appveyor_build_version } | Set-Content .\idzhook\resource2.rc
-  del .\idzhook\resource.rc
-  mv .\idzhook\resource2.rc .\idzhook\resource.rc
-  .\sgtools\GoRC.exe /fo .\idzhook\resource.res .\idzhook\resource.rc
-  .\sgtools\ResourceHacker.exe -open .\_build64\idzhook\idzhook.dll -save .\_build64\idzhook\idzhook_ver.dll -action add -res .\idzhook\resource.res
-  del .\_build64\idzhook\idzhook.dll
-  mv .\_build64\idzhook\idzhook_ver.dll .\_build64\idzhook\idzhook.dll
+﻿(Get-Content .\resource.rc).replace('1.0.0.0',$env:appveyor_build_version) | Set-Content .\resource2.rc
+del .\resource.rc
+mv .\resource2.rc .\resource.rc
+.\sgtools\GoRC.exe /fo .\resource.res .\resource.rc
